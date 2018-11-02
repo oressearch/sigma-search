572c3f4ec1059c24e6792873ce202fd668f0126f
Clément DOUIN <clement.douin@gmail.com>
2018-11-02 11:37:45 +0100

    integrate home:contact-us

--- src/app/components/home/contact/Contact.tsx
+++ src/app/components/home/contact/Contact.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import Container from '../../container'
+
+// @ts-ignore
+import styles from './Contact.styl'
+
+// @ts-ignore
+import background from './images/contact.jpeg'
+
 // ------------------------------------------------------------------ # Public #
 
 export default function Contact() {
   return (
-    <h1>Contact us</h1>
+    <section className={styles.section}>
+      <img src={background} alt="" />
+      <Container className={styles.container}>
+        <h1>Contact us</h1>
+        <p>By countries</p>
+      </Container>
+    </section>
   )
 }
