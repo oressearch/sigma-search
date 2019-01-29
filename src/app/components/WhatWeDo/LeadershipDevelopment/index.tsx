7b5b02993670600ee767d200fe7d0a4b258346b9
Clément DOUIN <soywod@users.noreply.github.com>
2019-01-29 16:31:26 +0100

    update prague infos

--- src/app/components/WhatWeDo/LeadershipDevelopment/index.tsx
+++ src/app/components/WhatWeDo/LeadershipDevelopment/index.tsx
@@ -9,6 +9,8 @@ import styles from './styles.styl'
 // @ts-ignore
 import corner from './images/corner.png'
 // @ts-ignore
+import flagCZ from './images/flag-cz.png'
+// @ts-ignore
 import flagDE from './images/flag-de.png'
 // @ts-ignore
 import flagFR from './images/flag-fr.png'
@@ -32,7 +34,7 @@ export default function LeadershipDevelopment() {
           Leadership Development <br />
           &amp; Organisation Consultancy
         </h2>
-        <p></p>
+        <p />
 
         <div className={styles.content}>
           <div className={styles.leftSide}>
@@ -41,53 +43,77 @@ export default function LeadershipDevelopment() {
               Leadership Development
             </h3>
             <p>
-              Sigma Search Partners offer advice and consultancy to
-              corporations on creating and implementing an education and
-              leadership development capability that is aligned with the
-              company's goals and objectives, including:
+              Sigma Search Partners offer advice and consultancy to corporations
+              on creating and implementing an education and leadership
+              development capability that is aligned with the company's goals
+              and objectives, including:
             </p>
             <ul>
               <li>
-                The definition of the processes whereby leadership
-                development is aligned with talent pool planning,
-                succession planning and building corporate bench strength
-                for leadership roles.
+                The definition of the processes whereby leadership development
+                is aligned with talent pool planning, succession planning and
+                building corporate bench strength for leadership roles.
               </li>
               <li>
-                The definition of the framework, plans and processes for
-                the development and coaching of effective leaders for the
-                business.
+                The definition of the framework, plans and processes for the
+                development and coaching of effective leaders for the business.
               </li>
             </ul>
             <p>
-              These Services are offered mainly by partners in Germany,
-              Italy, France, UK and the USA; further details are available
-              on their respective web sites.
+              These Services are offered mainly by partners in Germany, Italy,
+              France, UK and the USA; further details are available on their
+              respective web sites.
             </p>
 
             <ul className={styles.countries}>
               <li>
                 <img src={flagDE} alt="" />
-                <a href="https://www.odigos.de/index.php/en/your-human-resource-partner/" target="_blank">
+                <a
+                  href="https://www.odigos.de/index.php/en/your-human-resource-partner/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.odigos.de
                 </a>
               </li>
               <li>
                 <img src={flagFR} alt="" />
-                <a href="http://www.oressearch.com/" target="_blank">
+                <a
+                  href="http://www.oressearch.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.oressearch.com
                 </a>
               </li>
               <li>
                 <img src={flagGB} alt="" />
-                <a href="http://www.simmondsinternational.com/" target="_blank">
+                <a
+                  href="http://www.simmondsinternational.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.simmondsinternational.com
                 </a>
               </li>
               <li>
                 <img src={flagUS} alt="" />
-                <a href="https://www.leanhrconsult.com/" target="_blank">
+                <a
+                  href="https://www.leanhrconsult.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.leanhrconsult.com
+                </a>
+              </li>
+              <li>
+                <img src={flagCZ} alt="" />
+                <a
+                  href="http://www.touchdown.cz/"
+                  target="_blank"
+                  rel="author noopener"
+                >
+                  www.touchdown.cz
                 </a>
               </li>
             </ul>
@@ -100,53 +126,77 @@ export default function LeadershipDevelopment() {
             </h3>
             <p>
               Sigma Search Partners provide guidance to corporate general
-              management on building an organisational capability to meet
-              the corporation’s goals and objectives including:
+              management on building an organisational capability to meet the
+              corporation’s goals and objectives including:
             </p>
             <ul>
               <li>
-                Definition of the optimal organisation and leadership
-                skills and abilities required to meet the goals and
-                objectives.
+                Definition of the optimal organisation and leadership skills and
+                abilities required to meet the goals and objectives.
               </li>
               <li>
                 Creation of a talent management framework that includes
                 executive assessment, coaching and development planning,
-                succession planning and executive hiring and selection
-                models that will ensure the right people, processes and
-                structure are in place to produce and maintain excellent
-                business results.
+                succession planning and executive hiring and selection models
+                that will ensure the right people, processes and structure are
+                in place to produce and maintain excellent business results.
               </li>
             </ul>
             <p>
-              These Services are offered mainly by partners in Germany,
-              Italy, France, UK and the USA; further details are available
-              on their respective web sites.
+              These Services are offered mainly by partners in Germany, Italy,
+              France, UK and the USA; further details are available on their
+              respective web sites.
             </p>
 
             <ul className={styles.countries}>
               <li>
                 <img src={flagDE} alt="" />
-                <a href="https://www.odigos.de/index.php/en/your-human-resource-partner/" target="_blank">
+                <a
+                  href="https://www.odigos.de/index.php/en/your-human-resource-partner/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.odigos.de
                 </a>
               </li>
               <li>
                 <img src={flagFR} alt="" />
-                <a href="http://www.oressearch.com/" target="_blank">
+                <a
+                  href="http://www.oressearch.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.oressearch.com
                 </a>
               </li>
               <li>
                 <img src={flagGB} alt="" />
-                <a href="http://www.simmondsinternational.com/" target="_blank">
+                <a
+                  href="http://www.simmondsinternational.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.simmondsinternational.com
                 </a>
               </li>
               <li>
                 <img src={flagUS} alt="" />
-                <a href="https://www.leanhrconsult.com/" target="_blank">
+                <a
+                  href="https://www.leanhrconsult.com/"
+                  target="_blank"
+                  rel="author noopener"
+                >
                   www.leanhrconsult.com
+                </a>
+              </li>
+              <li>
+                <img src={flagCZ} alt="" />
+                <a
+                  href="http://www.touchdown.cz/"
+                  target="_blank"
+                  rel="author noopener"
+                >
+                  www.touchdown.cz
                 </a>
               </li>
             </ul>
